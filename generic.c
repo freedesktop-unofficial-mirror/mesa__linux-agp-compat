f9221297f9ddbdda82363ec15a6979754bdba318
Thomas Hellstrom <thomas-at-tungstengraphics-dot-com>
2006-10-20 15:10:15 +0200

    2.6.19 compatibility.

--- generic.c
+++ generic.c
@@ -27,7 +27,10 @@
  * TODO:
  * - Allocate more than order 0 pages to avoid too much linear map splitting.
  */
+#include <linux/version.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,19)
 #include <linux/config.h>
+#endif
 #include <linux/module.h>
 #include <linux/pci.h>
 #include <linux/init.h>
